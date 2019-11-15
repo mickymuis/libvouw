71f254640d9baeb4aab107bc987bdb6c12869973
Micky Faas <micky@edukitty.org>
2019-11-15 14:48:08 +0100

    Updated experiment 4096 snr

--- experiments/ril_4096_snr_test.sh
+++ experiments/ril_4096_snr_test.sh
@@ -4,19 +4,19 @@ export outfile="output_snr_4096.txt"
 
 function run {
     echo "Running tests with SNR=$1"
-    ../build/ril -rw=4096 -rh=4096 -rr=$1 -ru=70:$2 -rs=10:128000 -n2 -e 2> /dev/null | tail -n1 >> $outfile
+    ../build/ril -rw=4096 -rh=4096 -rr=$1 -ru=90:$2 -rs=10:128000 -n2 -e 2> /dev/null | tail -n1 >> $outfile
 }
 
 echo "# $outfile" > $outfile
 
 run .05 100
 run .1 100
-run .2 150
-run .3 150
-run .4 250
-run .5 250
-run .6 450
-run .7 600
-run .8 800
+run .2 200
+run .3 300
+run .4 400
+run .5 400
+run .6 600
+run .7 900
+run .8 900
 
 
